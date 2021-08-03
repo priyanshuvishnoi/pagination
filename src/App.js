f44188a185f3e5a9fbab5b84f9912479890d2d58
Priyanshu Vishnoi <priyanshuvishnoi990@gmail.com>
2021-08-03 15:25:37 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { Fragment, useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getData = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      `https://reqres.in/api/users?page=${page}`
+    );
+
+    setUsers(response.data.data);
+    setLoading(false);
+  };
+
+  const changePage = async action => {
+    switch (action) {
+      case '+':
+        setPage(value => value + 1);
+        break;
+      case '-':
+        setPage(value => value - 1);
+        break;
+      default:
+        break;
+    }
+    await getData();
+  };
+
+  useEffect(() => getData(), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? (
+        <div className="loader"></div>
+      ) : (
+        <Fragment>
+          <div className="data">
+            {users.map(user => (
+              <div className="user" key={user.id}>
+                <p>First Name: {user.first_name}</p>
+                <p>Last Name: {user.last_name}</p>
+                <p>Email: {user.email}</p>
+              </div>
+            ))}
+          </div>
+          <div className="page-control">
+            <button onClick={_ => changePage('-')} disabled={page === 1}>
+              &lt; Prev Page
+            </button>
+            <span>{page}</span>
+            <button onClick={_ => changePage('+')} disabled={page === 2}>
+              &gt; Next Page
+            </button>
+          </div>
+        </Fragment>
+      )}
     </div>
   );
 }
